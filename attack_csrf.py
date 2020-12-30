099cc0f3eaba17c68f8c3dbd0c08ce9c88922092
RonenZubkov <ronenownz@gmail.com>
2020-12-31 01:28:45 +0200

    Added Lfi / RFI Attack

--- attack_csrf.py
+++ attack_csrf.py
@@ -5,9 +5,4 @@
 """
 
 def csrf_attack(driver, target_url):
-    # print(driver, target_ur)
-    driver.get(target_url)
-    # if target_url == 'http://34.192.19.10/csrf_1.php?password_new=aaaa&password_conf=aaaa&action=change':
-    #     password_new = driver.find_element_by_id("password_new").send_keys('bugbugbug')
-    #     password_conf = driver.find_element_by_id("password_conf").send_keys('bugbugbug')
-
+    pass
